b589bf81223cb043a95aa05876124c9c8eaccc98
Daniel Blokus <daniel.blokus@gmail.com>
2018-11-05 19:58:01 +0100

    Add constant with a basic auth endpoint path

--- src/main/java/utils/WebServicePaths.java
+++ src/main/java/utils/WebServicePaths.java
@@ -4,6 +4,7 @@ public final class WebServicePaths {
 
 	public static final String EXAMPLE_ENDPOINT_PATH = "/example";
 	public static final String INVALID_ENDPOINT_PATH = "/something/else";
+	public static final String BA_ENDPOINT_PATH = "/basic/auth";
 
 	private WebServicePaths() {
 
