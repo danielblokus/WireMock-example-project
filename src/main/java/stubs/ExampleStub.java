9e50c1d36a47eda9140d33639625a6ff52cbb474
Daniel Blokus <daniel.blokus@gmail.com>
2018-11-06 21:07:03 +0100

    Add a stub method with a fixed delayed

--- src/main/java/stubs/ExampleStub.java
+++ src/main/java/stubs/ExampleStub.java
@@ -1,18 +1,17 @@
 package stubs;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
-import static utils.WebServicePaths.EXAMPLE_ENDPOINT_PATH;
-import static utils.WebServicePaths.INVALID_ENDPOINT_PATH;
 import static utils.HttpStatusCodes.NOT_FOUND;
+import static utils.WebServicePaths.*;
 
 public class ExampleStub {
 
 	private static final String CONTENT_TYPE_HEADER_KEY = "Content-Type";
 	private static final String TEXT_PLAIN_HEADER_VALUE = "text/plain";
+	private static final String bodyResponse = "Hello";
+	private static final int FIXED_DELAY_IN_MILLISECONDS = 2000;
 
 	public void stubExampleResponse() {
-		String bodyResponse = "Hello";
-
 		stubFor(get(EXAMPLE_ENDPOINT_PATH)
 			.willReturn(aResponse()
 				.withHeader(CONTENT_TYPE_HEADER_KEY, TEXT_PLAIN_HEADER_VALUE)
@@ -22,5 +21,12 @@ public class ExampleStub {
 	public void stubNotFoundResponse() {
 		stubFor(get(INVALID_ENDPOINT_PATH)
 			.willReturn(status(NOT_FOUND)));
+	}
+
+	public void stubFixedDelayResponse() {
+		stubFor(get(DELAYED_ENDPOINT_PATH)
+			.willReturn(aResponse()
+				.withBody(bodyResponse)
+				.withFixedDelay(FIXED_DELAY_IN_MILLISECONDS)));
 	}
 }
--- src/main/java/utils/WebServicePaths.java
+++ src/main/java/utils/WebServicePaths.java
@@ -5,6 +5,7 @@ public final class WebServicePaths {
     public static final String EXAMPLE_ENDPOINT_PATH = "/example";
     public static final String INVALID_ENDPOINT_PATH = "/something/else";
     public static final String BA_ENDPOINT_PATH = "/basic/auth";
+    public static final String DELAYED_ENDPOINT_PATH = "/delayed";
 
     private WebServicePaths() {
 
