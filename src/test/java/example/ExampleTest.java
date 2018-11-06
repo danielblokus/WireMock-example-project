9fea6ddc389229138237b06286eec1d7f61ed320
Daniel Blokus <daniel.blokus@gmail.com>
2018-11-06 21:13:58 +0100

    Reformat code

--- src/test/java/example/ExampleTest.java
+++ src/test/java/example/ExampleTest.java
@@ -3,8 +3,8 @@ package example;
 import io.restassured.http.ContentType;
 import org.junit.Test;
 import stubs.ExampleStub;
-import utils.WebServicePaths;
 import utils.HttpStatusCodes;
+import utils.WebServicePaths;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
@@ -13,60 +13,59 @@ import static utils.WebServicePaths.INVALID_ENDPOINT_PATH;
 
 public class ExampleTest extends BasicTest {
 
-	private ExampleStub exampleStub = new ExampleStub();
+    private static final long FIXED_DELAY_IN_MILLISECONDS = 2500L;
+    private static final String VALID_BODY_RESPONSE = "Hello";
+    private ExampleStub exampleStub = new ExampleStub();
 
-	private static final long FIXED_DELAY_IN_MILLISECONDS = 2500L;
-	private static final String VALID_BODY_RESPONSE = "Hello";
+    @Test
+    public void checkCorrectResponse() {
+        exampleStub.stubExampleResponse();
 
-	@Test
-	public void checkCorrectResponse() {
-		exampleStub.stubExampleResponse();
+        given()
+        .when()
+            .get(WebServicePaths.EXAMPLE_ENDPOINT_PATH)
+        .then()
+            .assertThat()
+                .body(equalTo(VALID_BODY_RESPONSE))
+            .and()
+                .statusCode(HttpStatusCodes.SUCCESS);
+    }
 
-		given()
-		.when()
-			.get(WebServicePaths.EXAMPLE_ENDPOINT_PATH)
-		.then()
-			.assertThat()
-				.body(equalTo(VALID_BODY_RESPONSE))
-			.and()
-				.statusCode(HttpStatusCodes.SUCCESS);
-	}
+    @Test
+    public void checkInvalidPath() {
+        exampleStub.stubNotFoundResponse();
 
-	@Test
-	public void checkInvalidPath() {
-		exampleStub.stubNotFoundResponse();
+        given()
+        .when()
+            .get(INVALID_ENDPOINT_PATH)
+        .then()
+            .assertThat()
+            .statusCode(HttpStatusCodes.NOT_FOUND);
+    }
 
-		given()
-		.when()
-			.get(INVALID_ENDPOINT_PATH)
-		.then()
-			.assertThat()
-				.statusCode(HttpStatusCodes.NOT_FOUND);
-	}
+    @Test
+    public void checkResponseHeader() {
+        exampleStub.stubExampleResponse();
 
-	@Test
-	public void checkResponseHeader() {
-		exampleStub.stubExampleResponse();
+        given()
+        .when()
+            .get(WebServicePaths.EXAMPLE_ENDPOINT_PATH)
+        .then()
+            .assertThat()
+            .contentType(ContentType.TEXT);
+    }
 
-		given()
-		.when()
-			.get(WebServicePaths.EXAMPLE_ENDPOINT_PATH)
-		.then()
-			.assertThat()
-				.contentType(ContentType.TEXT);
-	}
+    @Test
+    public void checkResponseAfterFixedDelay() {
+        exampleStub.stubFixedDelayResponse();
 
-	@Test
-	public void checkResponseAfterFixedDelay() {
-		exampleStub.stubFixedDelayResponse();
-
-		given()
-		.when()
-			.get(WebServicePaths.DELAYED_ENDPOINT_PATH)
-		.then()
-			.assertThat()
-				.body(equalTo(VALID_BODY_RESPONSE))
-			.and()
-				.time(lessThan(FIXED_DELAY_IN_MILLISECONDS));
-	}
+        given()
+        .when()
+            .get(WebServicePaths.DELAYED_ENDPOINT_PATH)
+        .then()
+            .assertThat()
+                .body(equalTo(VALID_BODY_RESPONSE))
+            .and()
+                .time(lessThan(FIXED_DELAY_IN_MILLISECONDS));
+    }
 }
