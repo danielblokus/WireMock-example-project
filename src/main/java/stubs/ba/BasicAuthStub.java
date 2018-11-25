7489dffa502627f83ba5db479de9daa331a6df94
Daniel Blokus <daniel.blokus@gmail.com>
2018-11-25 22:19:57 +0100

    Clean BasicAuthStub class

--- src/main/java/stubs/ba/BasicAuthStub.java
+++ src/main/java/stubs/ba/BasicAuthStub.java
@@ -6,7 +6,7 @@ import static utils.WebServicePaths.BA_ENDPOINT_PATH;
 
 public class BasicAuthStub {
 
-    private final String bodyResponse = "Hello BA!";
+    private static final String BODY_RESPONSE = "Hello BA!";
     private static final String VALID_USER_NAME = "admin@danielblokus.github.io";
     private static final String VALID_PASSWORD = "12345678";
 
@@ -14,7 +14,7 @@ public class BasicAuthStub {
         stubFor(get(BA_ENDPOINT_PATH)
                 .withBasicAuth(VALID_USER_NAME, VALID_PASSWORD)
                 .willReturn(aResponse()
-                        .withBody(bodyResponse)
+                        .withBody(BODY_RESPONSE)
                         .withStatus(SUCCESS)));
     }
 }
