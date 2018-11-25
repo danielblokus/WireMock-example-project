13cc179429551062170f62730881573db3b4741b
Daniel Blokus <daniel.blokus@gmail.com>
2018-11-25 22:20:27 +0100

    Cleanup util class

--- src/main/java/utils/HttpHeaders.java
+++ src/main/java/utils/HttpHeaders.java
@@ -2,10 +2,8 @@ package utils;
 
 public final class HttpHeaders {
 
-    //keys
     public static final String CONTENT_TYPE_HEADER_KEY = "Content-Type";
 
-    //values
     public static final String JSON_HEADER_VALUE = "application/json";
 
     private HttpHeaders() {}
