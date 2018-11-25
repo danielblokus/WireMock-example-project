d699425c57f434b35bccfb2f177f44a55f8db51f
Daniel Blokus <daniel.blokus@gmail.com>
2018-11-25 22:17:42 +0100

    Add ImagesStub

---
+++ src/main/java/stubs/ba/ImagesStub.java
@@ -0,0 +1,25 @@
+package stubs.ba;
+
+import utils.HttpHeaders;
+import utils.HttpStatusCodes;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static utils.WebServicePaths.IMAGES_ENDPOINT_PATH;
+
+public class ImagesStub {
+
+    private static final String JSON_RESPONSE_PATH = "stubs/ba/imagesResponse.json";
+    private static final String VALID_USER_NAME = "admin@danielblokus.github.io";
+    private static final String VALID_PASSWORD = "12345678";
+
+    public void stubResponse() {
+        stubFor(get(IMAGES_ENDPOINT_PATH)
+                .withBasicAuth(VALID_USER_NAME, VALID_PASSWORD)
+                .willReturn(aResponse()
+                        .withStatus(HttpStatusCodes.SUCCESS)
+                        .withHeader(HttpHeaders.CONTENT_TYPE_HEADER_KEY, HttpHeaders.JSON_HEADER_VALUE)
+                        .withBodyFile(JSON_RESPONSE_PATH)));
+    }
+}
--- src/main/java/utils/WebServicePaths.java
+++ src/main/java/utils/WebServicePaths.java
@@ -6,6 +6,7 @@ public final class WebServicePaths {
     public static final String INVALID_ENDPOINT_PATH = "/something/else";
     public static final String BA_ENDPOINT_PATH = "/basic/auth";
     public static final String DELAYED_ENDPOINT_PATH = "/delayed";
+    public static final String IMAGES_ENDPOINT_PATH = "/images";
 
     private WebServicePaths() {
 
