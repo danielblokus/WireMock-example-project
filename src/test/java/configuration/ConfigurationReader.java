19994f0fe5c4c7df87cf5a7c23e4565b9879935d
danielblokus <daniel.blokus@gmail.com>
2019-04-01 19:04:13 +0200

    Create ConfigurationReader.java

---
+++ src/test/java/configuration/ConfigurationReader.java
@@ -0,0 +1,40 @@
+package configuration;
+
+import java.io.*;
+import java.util.Objects;
+import java.util.Properties;
+
+public class ConfigurationReader {
+
+	private static final String PATH_TO_CONFIGURATION_FILE = "configuration.properties";
+
+	private Properties properties;
+
+	public ConfigurationReader() {
+		readPropertiesFromConfigurationFile();
+	}
+
+	private void readPropertiesFromConfigurationFile() {
+		properties = new Properties();
+		try {
+			properties.load(getConfigurationFilePath());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private BufferedReader getConfigurationFilePath() {
+		return new BufferedReader(new InputStreamReader(Objects.requireNonNull(getClass().getClassLoader()
+				.getResourceAsStream(PATH_TO_CONFIGURATION_FILE))));
+	}
+
+	public String getBasicAuth() {
+		String path = properties.getProperty("basicAuthUserName");
+		if (path != null) {
+			return path;
+		}
+		else {
+			throw new RuntimeException("basicAuthUserName not specified in the configuration.properties file");
+		}
+	}
+}
